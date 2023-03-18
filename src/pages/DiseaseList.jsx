a10ffc3fc1dd7110d139d48047c37e617bc45c8e
it21096266 <it21096266@my.sliit.lk>
2023-03-18 12:26:21 +0530

    DiseaseList page

--- src/pages/DiseaseList.jsx
+++ src/pages/DiseaseList.jsx
@@ -5,17 +5,17 @@ import Helmet from "../components/Helmet/Helmet";
 const DiseaseList = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-    <main className="mt-1 p-12 w-full ">
-      <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Helmet title="Address List">
+        <main className="mt-1 p-12 w-full ">
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Helmet title="Address List">
             
             <div>DiseaseList</div>
 
-         </Helmet>
-          </div>
-        </div>
-    </main>
+                    </Helmet>
+                </div>
+            </div>
+        </main>
     </div>
   )
 }
