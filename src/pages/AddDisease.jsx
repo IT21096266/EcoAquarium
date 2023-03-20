facfba744bdefebc780e62e7d541d7aed40f3371
W.M.P.I.Dharmagunawardana <it21132346@my.sliit.lk>
2023-03-21 01:53:21 +0530

    create-operation-done/DB connection-done

--- src/pages/AddDisease.jsx
+++ src/pages/AddDisease.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef  } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "../Styles/styles";
 import Helmet from "../components/Helmet/Helmet";
 
@@ -7,41 +7,39 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const AddDisease = () => {
+  const { diseaseID } = useParams();
 
-    const { diseaseID } = useParams();
+  const [diseaseName, setDiseaseName] = useState("");
+  const [diseaseCauses, setDiseaseCauses] = useState("");
+  const [diseasePrevention, setDiseasePrevention] = useState("");
+  const [diseaseTreatment, setDiseaseTreatment] = useState("");
+  const [message, setMessage] = useState({ error: false, msg: "" });
 
-    const [diseaseName, setDiseaseName] = useState("");
-    const [diseaseCauses, setDiseaseCauses] = useState("");
-    const [diseasePrevention, setDiseasePrevention] = useState("");
-    const [diseaseTreatment, setDiseaseTreatment] = useState("");
-    const [message, setMessage] = useState({ error: false, msg: "" });
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      const newDisease = {
-        diseaseName,
-        diseaseCauses,
-        diseasePrevention,
-        diseaseTreatment,
-      };
-      console.log(newDisease);
-      try {
-        await DiseaseDataService.addDisease(newDisease);
-        alert("Disease added successfully!");
-        navigate("/diseaseList");
-      } catch (err) {
-        setMessage({ error: false, msg: err.message });
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newDisease = {
+      diseaseName,
+      diseaseCauses,
+      diseasePrevention,
+      diseaseTreatment,
     };
+    console.log(newDisease);
+    try {
+      await DiseaseDataService.addDisease(newDisease);
+      alert("Disease added successfully!");
+      navigate("/diseaseList");
+    } catch (err) {
+      setMessage({ error: false, msg: err.message });
+    }
+  };
 
   return (
     <div className="bg-primary w-full overflow-hidden">
-        <main className="mt-1 p-12 w-full ">
-            <div className={`bg-primary ${styles.flexStart}`}>
-                <div className={`${styles.boxWidth}`}>
-                    <Helmet title="Disease List">
-            
-                    {console.log("Address ID: ", diseaseID)}
+      <main className="mt-1 p-12 w-full ">
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Helmet title="Disease List">
+              {console.log("Address ID: ", diseaseID)}
               <div className="mt-10 sm:mt-0">
                 <div className="md:grid md:grid-cols-3 md:gap-6">
                   <div className="md:col-span-1">
@@ -63,7 +61,6 @@ const AddDisease = () => {
                       <div className="shadow overflow-hidden sm:rounded-md">
                         <div className="px-4 py-5 bg-white sm:p-6">
                           <div className="grid grid-cols-6 gap-6">
-                            
                             <div className="col-span-6 sm:col-span-5">
                               <label htmlFor="first_name" className="formLable">
                                 Disease Name
@@ -75,7 +72,7 @@ const AddDisease = () => {
                                 maxLength="10"
                                 defaultValue={diseaseName}
                                 onSelect={(e) => {
-                                    setDiseaseName(e.target.value);
+                                  setDiseaseName(e.target.value);
                                 }}
                                 required
                                 className={`${styles.ADtxt}`}
@@ -93,7 +90,7 @@ const AddDisease = () => {
                                 maxLength="15"
                                 defaultValue={diseaseCauses}
                                 onSelect={(e) => {
-                                    setDiseaseCauses(e.target.value);
+                                  setDiseaseCauses(e.target.value);
                                 }}
                                 required
                                 className={`${styles.ADtxt}`}
@@ -101,7 +98,10 @@ const AddDisease = () => {
                             </div>
 
                             <div className="col-span-6 sm:col-span-5">
-                              <label htmlFor="disease_prevention" className="formLable">
+                              <label
+                                htmlFor="disease_prevention"
+                                className="formLable"
+                              >
                                 Disease Prevention
                               </label>
                               <input
@@ -111,7 +111,7 @@ const AddDisease = () => {
                                 maxLength="15"
                                 defaultValue={diseasePrevention}
                                 onSelect={(e) => {
-                                    setDiseasePrevention(e.target.value);
+                                  setDiseasePrevention(e.target.value);
                                 }}
                                 required
                                 className={`${styles.ADtxt}`}
@@ -119,7 +119,10 @@ const AddDisease = () => {
                             </div>
 
                             <div className="col-span-6 sm:col-span-3 lg:col-span-5">
-                              <label htmlFor="disease_treatment" className="formLable">
+                              <label
+                                htmlFor="disease_treatment"
+                                className="formLable"
+                              >
                                 Disease Treatment
                               </label>
                               <input
@@ -129,13 +132,12 @@ const AddDisease = () => {
                                 maxLength="15"
                                 defaultValue={diseaseTreatment}
                                 onSelect={(e) => {
-                                    setDiseaseTreatment(e.target.value);
+                                  setDiseaseTreatment(e.target.value);
                                 }}
                                 required
                                 className={`${styles.ADtxt}`}
                               />
                             </div>
-
                           </div>
                         </div>
                         <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
@@ -159,12 +161,12 @@ const AddDisease = () => {
                   </div>
                 </div>
               </div>
-                    </Helmet>
-                </div>
-            </div>
-        </main>
+            </Helmet>
+          </div>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default AddDisease
+export default AddDisease;
--- src/pages/AddTreatment.jsx
+++ src/pages/AddTreatment.jsx
@@ -1,21 +1,94 @@
-import React from 'react'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { async } from "@firebase/util";
+import treatment_data_services from "../services/treatment_services";
+import styles from "../Styles/styles";
+import Helmet from "../components/Helmet/Helmet";
 
 const AddTreatment = () => {
+  const [product_name, setProduct_name] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newTreatment = {
+      product_name,
+      description,
+    };
+    console.log(newTreatment);
+    try {
+      await treatment_data_services.addTreatment(newTreatment);
+      alert("New Treatment Added Successfully!");
+      //navigate("/stocklist");
+    } catch (err) {
+      setMessage({ error: true, msg: err.message });
+    }
+    setProduct_name("");
+    setDescription("");
+  };
+
   return (
     <div className="bg-primary w-full overflow-hidden">
-        <main className="mt-1 p-12 w-full ">
-            <div className={`bg-primary ${styles.flexStart}`}>
-                <div className={`${styles.boxWidth}`}>
-                    <Helmet title="Treatment Form">
-            
-            <div>Treatment Form</div>
+      <main className="mt-1 p-12 w-full ">
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Helmet title="Treatment Form">
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label
+                      for="small-input"
+                      class="block mb-2 text-sm font-medium text-gray-900 text-black"
+                    >
+                      Product Name
+                    </label>
+                    <input
+                    required
+                    defaultValue={product_name}
+                    onSelect={(e) => {
+                      setProduct_name(e.target.value);
+                    }}
+                      type="text"
+                      id="small-input"
+                      class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    />
+                  </div>
 
-                    </Helmet>
-                </div>
-            </div>
-        </main>
+                  <label
+                    for="message"
+                    class="block mb-2 text-sm font-medium text-gray-900 text-black"
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    id="message"
+                    rows="4"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Leave a comment..."
+                    required
+                    defaultValue={description}
+                    onSelect={(e) =>{
+                      setDescription(e.target.value)
+                    }}
+                  ></textarea>
+
+                  <div>
+                    <button
+                      type="submit"
+                      class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </Helmet>
+          </div>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default AddTreatment
+export default AddTreatment;
