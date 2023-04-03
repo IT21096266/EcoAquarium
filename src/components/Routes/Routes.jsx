0e74e26e0d04ed9df5f7be3964668d4b0d80b155
IT21080562 <87889025+IT21080562@users.noreply.github.com>
2023-04-03 20:18:57 +0530

    CHG | Add fish game calculator

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import { DiseaseList, AddDisease, DiseaseUpdate, AddTreatment, Create, Upload, ViewPosts,TreatmentList } from '../../pages'
+import {
+	DiseaseList,
+	AddDisease,
+	DiseaseUpdate,
+	AddTreatment,
+	Create,
+	Upload,
+	ViewPosts,
+	TreatmentList,
+} from "../../pages";
 
 import { MainContainer } from "../index";
+import Calculator from "../Calculator/Calculator";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/*" element={<MainContainer />} />
-      <Route path="/diseaseList" element={<DiseaseList />} />
-      <Route path="/diseaseUpdate" element={<DiseaseUpdate />} />
-      <Route path="/addDisease" element={<AddDisease />} />
-      <Route path="/diseaseList/diseaseUpdate/:diseaseID" element={<DiseaseUpdate />} />
-      <Route path='/addtreatment' element={<AddTreatment/> }/>
-      <Route path="/create" element={<Create />} />
-      <Route path="/upload" element={<Upload />} />
-      <Route path="/viewposts" element={<ViewPosts />} />  
-      <Route path="/treatmentlist" element={<TreatmentList/>}/>  
-    
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/*" element={<MainContainer />} />
+			<Route path="/diseaseList" element={<DiseaseList />} />
+			<Route path="/diseaseUpdate" element={<DiseaseUpdate />} />
+			<Route path="/addDisease" element={<AddDisease />} />
+			<Route
+				path="/diseaseList/diseaseUpdate/:diseaseID"
+				element={<DiseaseUpdate />}
+			/>
+			<Route path="/addtreatment" element={<AddTreatment />} />
+			<Route path="/create" element={<Create />} />
+			<Route path="/upload" element={<Upload />} />
+			<Route path="/viewposts" element={<ViewPosts />} />
+			<Route path="/treatmentlist" element={<TreatmentList />} />
+			<Route path="/calculator" element={<Calculator />} />
+		</Routes>
+	);
 };
 export default AppRoutes;
