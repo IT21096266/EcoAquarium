85f49d3fa576c0925e4b45df13dee00da6e40885
W.M.P.I.Dharmagunawardana <it21132346@my.sliit.lk>
2023-05-12 23:28:45 +0530

    pdf genaration done

--- src/pages/TreatmentView.jsx
+++ src/pages/TreatmentView.jsx
@@ -20,6 +20,7 @@ import { actionType } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
 import { doc } from "firebase/firestore";
 import { data } from "autoprefixer";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 
 const TreatmentView = () => {
   const [treatment, setTreatment] = useState([]);
@@ -40,35 +41,36 @@ const TreatmentView = () => {
         <main className="mt-1 p-12 w-full ">
           <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
-              <div
-                className={`w-full items-center justify-evenly gap-5 my-12 flex flex-wrap`}
-              >
-                {treatment.map((doc, index) => {
-                  return (
-                    <div className="max-w-sm col-span-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-60">
-                      <a href="#">
-                        <img
-                          src={doc.imageAsset}
-                          alt="treatment image"
-                          className="rounded-lg h-60"
-                        />
-                      </a>
-                      <div className="p-5">
+                <div
+                  className={`w-full items-center justify-evenly gap-5 my-12 flex flex-wrap`}
+                >
+                  {treatment.map((doc) => {
+                    return (
+                      <div className="max-w-sm col-span-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-60">
                         <a href="#">
-                          <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {doc.product_name.substring(0, 17)}
-                            {doc.product_name.length > 17 ? "..." : ""}
-                          </h5>
+                          <img
+                            src={doc.imageAsset}
+                            alt="treatment image"
+                            className="rounded-lg h-60"
+                          />
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                          {doc.description.substring(0, 45)}
-                          {doc.description.length > 45 ? "..." : ""}
-                        </p>
+                        <div className="p-5">
+                          <a href="#">
+                            <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">
+                              {doc.product_name.substring(0, 17)}
+                              {doc.product_name.length > 17 ? "..." : ""}
+                            </h5>
+                          </a>
+                          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                            {doc.description.substring(0, 45)}
+                            {doc.description.length > 45 ? "..." : ""}
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
-              </div>
+                    );
+                  })}
+                </div>
+                
             </div>
           </div>
         </main>
