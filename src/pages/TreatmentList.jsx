b0a1f8143569284f1262f30ba9846b50c71a4d2d
W.M.P.I.Dharmagunawardana <it21132346@my.sliit.lk>
2023-05-15 23:31:32 +0530

    a minor change

--- src/pages/TreatmentList.jsx
+++ src/pages/TreatmentList.jsx
@@ -138,7 +138,7 @@ const TreatmentList = () => {
                     alt="treatment image"
                     className="rounded-lg h-56"
                   />
-                  <div class="absolute top-0 right-0 flex items-end">
+                  <div class="absolute top-0 right-0 flex items-end mt-5 mr-5">
                     <button
                       type="button"
                       class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
