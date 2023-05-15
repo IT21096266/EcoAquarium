9d5a2932d10c0628f1f095b0cc79cb01bf9fd9b3
IT21096266 <it21096266@my.sliit.lk>
2023-05-15 22:59:37 +0530

    Search function

--- src/pages/DiseaseView.jsx
+++ src/pages/DiseaseView.jsx
@@ -5,7 +5,6 @@ import styles from "../Styles/styles";
 import Helmet from "../components/Helmet/Helmet";
 
 const DiseaseView = () => {
-
   const [disease, setDisease] = useState([]);
 
   useEffect(() => {
@@ -20,9 +19,27 @@ const DiseaseView = () => {
   };
 
   // Navigate
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const navigateModel = () => {
     navigate("/model");
+  };
+
+  // Search bar
+  const [value, setValue] = useState("");
+  const [filterValue, setFilterValue] = useState([]);
+  const filterData = (e) => {
+    if (e.target.value != "") {
+      setValue(e.target.value);
+      const filterValue = disease.filter((o) =>
+        Object.keys(o).some((k) =>
+          String(o[k]).toLowerCase().includes(e.target.value.toLowerCase())
+        )
+      );
+      setFilterValue([...filterValue]);
+    } else {
+      setValue(e.target.value);
+      setTicket([...disease]);
+    }
   };
 
   return (
@@ -32,38 +49,85 @@ const DiseaseView = () => {
           <div className={`${styles.boxWidth}`}>
             <Helmet title="Disease List">
               <div>
-                <button
-                  className={`${styles.ADbtn}`}
-                  onClick={navigateModel}>
+                <button className={`${styles.ADbtn}`} onClick={navigateModel}>
                   Check Disease
                 </button>
               </div>
-        {disease.map((doc, index) => {
-                return (
-                    <div>
-                        <Link to={`diseaseDetails/${doc.id}`}>
-                            <div class="gap-5 my-4 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  
-                                        hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative">
-                                <img
+              <div className="w-96 float-right">
+                <label htmlFor="simple-search" className="sr-only">
+                  Search
+                </label>
+                <input
+                  value={value}
+                  onChange={filterData}
+                  type="text"
+                  id="simple-search"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm  block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+                  placeholder="Search"
+                  required
+                />
+              </div>
+
+              <section>
+                {value.length > 0
+                  ? filterValue.map((doc) => {
+                      return (
+                        <div>
+                          <Link to={`diseaseDetails/${doc.id}`}>
+                            <div
+                              className="gap-5 my-4 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  
+                                    hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative">
+                              <img
                                 src={doc.imageAsset}
                                 alt="disease image"
                                 className="rounded-lg h-56"/>
-                                <div class="flex flex-col justify-between p-10 leading-normal">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hidden">
-                                    {index + 1}
+
+                              <div className="flex flex-col justify-between p-10 leading-normal">
+                                {/* <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hidden">
+                                  {index + 1}
+                                </h5> */}
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                  {doc.diseaseName}
                                 </h5>
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    {doc.diseaseName}
-                                </h5>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                    {doc.diseaseDescription}
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                  {doc.diseaseDescription}
                                 </p>
-                                </div>
+                              </div>
                             </div>
-                        </Link>
-                    </div>
-                );
-              })}
+                          </Link>
+                        </div>
+                      );
+                    })
+                  : disease.map((doc, index) => {
+                      return (
+                        <div>
+                          <Link to={`diseaseDetails/${doc.id}`}>
+                            <div
+                              className="gap-5 my-4 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  
+                                          hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative">
+                              <img
+                                src={doc.imageAsset}
+                                alt="disease image"
+                                className="rounded-lg h-56"/>
+
+                              <div className="flex flex-col justify-between p-10 leading-normal">
+                                {/* <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hidden">
+                                  {index + 1}
+                                </h5> */}
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                  {doc.diseaseName}
+                                </h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                  {doc.diseaseDescription}
+                                </p>
+
+                              </div>
+                            </div>
+                          </Link>
+                        </div>
+                      );
+                    })}
+              </section>
             </Helmet>
           </div>
         </div>
